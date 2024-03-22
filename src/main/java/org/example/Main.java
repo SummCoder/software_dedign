1f69860e86c6747654c7547592c5f424554b8414
SummCoder <2404174187@qq.com>
2024-03-22 23:52:29 +0800

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+/**
+ * @author SummCoder
+ * @date 2024/03/22 23:51
+ */
+
 public class Main {
     public static void main(String[] args) {
         String casePath = args[0];
